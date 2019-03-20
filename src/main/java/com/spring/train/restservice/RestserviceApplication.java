b45e9e06326a3bd752dc087ca6ee86a1c3c42afd
p.puczka <p.puczka@gmail.com>
2019-03-20 21:28:52 +0100

    rest workshop Spring Boot App

---
+++ src/main/java/com/spring/train/restservice/RestserviceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.train.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestserviceApplication.class, args);
+	}
+
+}
