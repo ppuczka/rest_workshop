998d34f5c8bb655ea4a0e8f8f662c2c2654968d4
p.puczka <p.puczka@gmail.com>
2019-04-17 23:19:26 +0200

    rest project run o my mac

--- src/main/java/com/spring/train/restservice/repository/OrderRepository.java
+++ src/main/java/com/spring/train/restservice/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.spring.train.restservice.repository;
 
+
 import com.spring.train.restservice.entity.Order;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
